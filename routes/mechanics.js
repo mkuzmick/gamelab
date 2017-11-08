2893a67b9a92a2e67f665286d441aff12c9a3f14
LL-101 <ll-101@dhcp-140-247-38-105.fas.harvard.edu>
2017-11-08 10:19:35 -0500

    none

--- routes/mechanics.js
+++ routes/mechanics.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 
-var gameMechanics = [{name: 'Scotland Yard', imageUrl: 'images/Scotlandyard2.jpg', rationale: "This game would be cool to use because it's about Scotland."}, {name: 'Mah Jong', imageUrl: 'images/Mah-yongg_stones.jpg', rationale: "This game would be cool to use because it has tiles."}, {name: 'Hopscotch', imageUrl:'/images/hopscotch.jpg', rationale: 'Jumping is fun'}]
+var gameMechanics = [
+    {name: 'Scotland Yard', imageUrl: 'images/Scotlandyard2.jpg', rationale: "This game would be cool to use because it's about Scotland."},
+    {name: 'Mah Jong', imageUrl: 'images/Mah-yongg_stones.jpg', rationale: "This game would be cool to use because it has tiles."},
+    {name: 'Monopoly', imageUrl: 'images/monopoly.jpg', rationale: "Real estate is fun."}, 
+    {name: 'Hopscotch', imageUrl:'/images/hopscotch.jpg', rationale: 'Jumping is fun'}
+  ]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
