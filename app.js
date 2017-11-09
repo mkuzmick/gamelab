813333e2011031c885bec20c55d2eedbc5ffa961
mkuzmick <mkuzmick@gmail.com>
2017-11-08 21:57:22 -0500

    db added

--- app.js
+++ app.js
@@ -14,6 +14,24 @@ require('dotenv').config();
 
 var app = express();
 
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+// var mongoDB = process.env.MONGODB_URL;
+var mongoDB = process.env.MLAB_URL;
+
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
